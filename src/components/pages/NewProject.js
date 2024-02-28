9881c9df3beab32619079f6bee06b40ab49177ef
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-02-28 08:59:19 -0300

    :mag:API

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,24 @@
 import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css"
+
+import { useNavigate } from "react-router-dom"
+
 function NewProject (){
+
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    }).then((resp) => resp.json()).then((data) => {navigate("/projects", { message: "Projeto criado com sucesso!" });}).catch((err) => console.log(err))
+  }
 
   return (
     <div className={styles.newproject_container}>
@@ -10,7 +28,7 @@ function NewProject (){
       <p>
         Crie seu projeto para depois adicionar os serviços 
       </p>
-      <ProjectForm btnText="Criar Projeto"></ProjectForm>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"></ProjectForm>
     </div>
   )
 
